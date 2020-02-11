3cff55b85e06177dbc9506fdf4dc2ea06e9b37c9
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-02-11 00:43:34 -0700

    Configuración de variables de entrono para implementación de passport, seed para trabajar con las movies y users

--- movies-api/config/index.js
+++ movies-api/config/index.js
@@ -7,7 +7,12 @@ const config = {
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
   dbPass: process.env.DB_PASS,
-  dbHost: process.env.DB_HOST
+  dbHost: process.env.DB_HOST,
+  defaultAdminPass: process.env.DEFAULT_ADMIN_PASS,
+  defaultUserPass: process.env.DEFAULT_USER_PASS,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  adminApiKey: process.env.ADMIN_API_KEY,
+  userApiKey: process.env.USER_API_KEY
 }
 
 module.exports = { config }
