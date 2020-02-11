14eeea0f8d00001f922bffd2ad42b2a231c81a08
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-02-10 23:58:22 -0700

    Implementacion de rutas para creacion y eliminación de peliculas por usuario

--- movies-api/routes/userMovies.js
+++ movies-api/routes/userMovies.js
@@ -4,10 +4,10 @@ const UserMovieService = require('../services/userMovie');
 const validationHandler = require('../utils/middleware/validationHandler');
 // const { movieIdSchema } = require('../utils/schemas/movies');
 const { userIdSchema } = require('../utils/schemas/users');
-// const {
-//   userMovieIdSchema,
-//   userMovieCreateSchema
-// } = require('../utils/schemas/userMovie');
+const {
+  userMovieIdSchema,
+  userMovieCreateSchema
+} = require('../utils/schemas/userMovie');
 
 function userMovieApi(app) {
   const router = express.Router();
@@ -26,6 +26,46 @@ function userMovieApi(app) {
         res.status(200).json({
           data: userMovies,
           message: 'User movies listed.'
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+  router.post(
+    '/',
+    validationHandler(userMovieCreateSchema),
+    async (req, res, next) => {
+      const { body: userMovie } = req;
+      try {
+        const createdUserMovieId = await userMoviesSerivice.createUserMovies(
+          userMovie
+        );
+
+        res.status(201).json({
+          data: createdUserMovieId,
+          message: 'Movie added to user'
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+  router.delete(
+    '/:userMovieId',
+    validationHandler(userMovieIdSchema),
+    async (req, res, next) => {
+      const { userMovieId } = req.params;
+      try {
+        const deletedUserMovieId = await userMoviesSerivice.deleteUserMovie(
+          userMovieId
+        );
+
+        res.status(200).json({
+          data: deletedUserMovieId,
+          message: 'Move removed from user.'
         });
       } catch (error) {
         next(error);
