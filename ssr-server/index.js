f7263db2d9ec1eb458bacedf61c68e22c6ef8819
Paola Alapizco <pahoc.alapizco@gmail.com>
2020-02-12 12:53:45 -0700

    Implementación de autenticación den passport desde proxy ssr

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const passport = require("passport");
+const boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 
 const { config } = require("./config");
 
@@ -7,10 +11,50 @@ const app = express();
 // body parser
 app.use(express.json());
 
-// Login
-app.post("/auth/sing-in", async function(req, res, next) {});
+// cookie parser
+app.use(cookieParser());
 
-app.post("/auth/sing-up", async function(req, res, next) {});
+// Basic strategy
+require("./utils/auth/strategies/basic");
+
+// Login
+app.post("/auth/sing-in", async function(req, res, next) {
+  passport.authenticate("basic", function(error, data) {
+    try {
+      if (error || !data) next(boom.unauthorized());
+
+      req.login(data, { session: false }, async function(error) {
+        const { token, ...user } = data;
+        if (error) next(error);
+
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post("/auth/sing-up", async function(req, res, next) {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sing-up`,
+      method: "POST",
+      data: user
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Movies
 app.get("/movies", async function(req, res, next) {});
@@ -23,4 +67,3 @@ app.delete("/movies/:movieId", async function(req, res, next) {});
 app.get("/user-movies", async function(req, res, next) {});
 app.post("/user-movies", async function(req, res, next) {});
 app.delete("/user-movies/:userMovieId", async function(req, res, next) {});
-
